a942c9741746f5ec061c8d0e1c9183798e99dff1
Gaurav Chopra <gaurav.chopra13@gmail.com>
2021-05-26 22:38:43 +0530

    first commit

--- src/XmlTransformer.java
+++ src/XmlTransformer.java
@@ -13,13 +13,38 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class XmlTransformer {
 
-
+     
 
     public static  void  main(String... args) throws XPathExpressionException, IOException, SAXException, ParserConfigurationException {
         File file = new File("src/file.xml");
+        Map<String, String> attributes1 = new HashMap<>();
+        extracted(file, attributes1);
+        System.out.println(attributes1);
+
+        File file1 = new File("src/file1.xml");
+        Map<String, String> attributes2 = new HashMap<>();
+        extracted(file1, attributes2);
+        System.out.println(attributes2);
+
+        System.out.println("Difference in xmls::"+areEqualKeyValues(attributes2,attributes1));
+
+
+
+    }
+
+    private static Map<String, Boolean> areEqualKeyValues(Map<String, String> first, Map<String, String> second) {
+        return first.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey(),
+                        e -> !e.getValue().equals(second.get(e.getKey()))));
+    }
+
+    private static void extracted(File file, Map<String, String> attributes1) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
         XPath xPath =  XPathFactory.newInstance().newXPath();
         String expression = "//*[not(*)]";
 
@@ -30,8 +55,9 @@ public class XmlTransformer {
 
         NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
         for(int i = 0 ; i < nodeList.getLength(); i++) {
-            System.out.println(getXPath(nodeList.item(i)));
-            System.out.println(nodeList.item(i).getFirstChild().getNodeValue());
+            attributes1.put(getXPath(nodeList.item(i)),nodeList.item(i).getFirstChild().getNodeValue());
+
+
         }
     }
 
